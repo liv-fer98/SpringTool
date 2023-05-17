ed5a2ebea1322424af4a3c38a51b2310a092a205
User <User@DESKTOP-SF593TN>
2023-05-17 11:33:15 -0300

    Atualização

--- TechLine/src/main/java/br/com/loja/techline/domain/FormaDePagamento.java
+++ TechLine/src/main/java/br/com/loja/techline/domain/FormaDePagamento.java
@@ -1,5 +1,9 @@
 package br.com.loja.techline.domain;
 
+
 public class FormaDePagamento {
+	
+
+	
 
 }
--- TechLine/src/main/java/br/com/loja/techline/domain/Produto.java
+++ TechLine/src/main/java/br/com/loja/techline/domain/Produto.java
@@ -1,5 +1,78 @@
 package br.com.loja.techline.domain;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Produto {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private float preco;
+	private int qtdEstoque;
+	
+	@OneToMany(mappedBy = "produto", cascade = CascadeType.ALL)
+	private List<Fornecedor> fornecedores;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public int getQtdEstoque() {
+		return qtdEstoque;
+	}
+
+	public void setQtdEstoque(int qtdEstoque) {
+		this.qtdEstoque = qtdEstoque;
+	}
+
+	public List<Fornecedor> getFornecedores() {
+		return fornecedores;
+	}
+
+	public void setFornecedores(List<Fornecedor> fornecedores) {
+		this.fornecedores = fornecedores;
+	}
+	
+	
+	
+	
 
 }
