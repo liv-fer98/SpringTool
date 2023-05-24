614317b5968ef632e9b12b234a34007982c1b6b1
User <User@DESKTOP-SF593TN>
2023-05-23 22:28:06 -0300

    Atualização

---
+++ TechLine/src/main/java/br/com/loja/techline/repository/formaDePagamentoRepository.java
@@ -0,0 +1,9 @@
+package br.com.loja.techline.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.loja.techline.domain.FormaDePagamento;
+
+public interface formaDePagamentoRepository extends JpaRepository<FormaDePagamento, Long>{
+
+}
---
+++ TechLine/src/main/java/br/com/loja/techline/service/CarrinhoService.java
@@ -0,0 +1,8 @@
+package br.com.loja.techline.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarrinhoService {
+
+}
