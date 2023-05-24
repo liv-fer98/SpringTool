8ef6bb81f8bf222c69bec18d0156444a2ca6aa0f
User <User@DESKTOP-SF593TN>
2023-05-23 22:36:27 -0300

    Atualização

---
+++ TechLine/src/main/java/br/com/loja/techline/resources/CarrinhoResources.java
@@ -0,0 +1,10 @@
+package br.com.loja.techline.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/carrinhos")
+public class CarrinhoResources {
+
+}
