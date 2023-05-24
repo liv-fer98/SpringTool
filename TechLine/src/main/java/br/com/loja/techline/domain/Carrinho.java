e1568edfd6f9b3a0588c6d924f942c9f88baca6c
User <User@DESKTOP-SF593TN>
2023-05-23 22:13:30 -0300

    Atualização

--- TechLine/src/main/java/br/com/loja/techline/domain/Carrinho.java
+++ TechLine/src/main/java/br/com/loja/techline/domain/Carrinho.java
@@ -1,5 +1,68 @@
 package br.com.loja.techline.domain;
 
+import java.util.List;
+
+import javax.xml.crypto.Data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Carrinho {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Double valorTotal;
+	private Data dataEmissao;
+	private Data dataEntrada;
+	
+	private List<Item> itens;
+	
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public Data getDataEmissao() {
+		return dataEmissao;
+	}
+
+	public void setDataEmissao(Data dataEmissao) {
+		this.dataEmissao = dataEmissao;
+	}
+
+	public Data getDataEntrada() {
+		return dataEntrada;
+	}
+
+	public void setDataEntrada(Data dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+
+	public List<Item> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<Item> itens) {
+		this.itens = itens;
+	}
+	
+	
 
 }
--- TechLine/src/main/java/br/com/loja/techline/domain/FormaDePagamento.java
+++ TechLine/src/main/java/br/com/loja/techline/domain/FormaDePagamento.java
@@ -1,9 +1,42 @@
 package br.com.loja.techline.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class FormaDePagamento {
 	
-
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
+	
+	public enum Tipos{
+		 
+		Dinheiro("D"),
+		Credito("C"),
+		debito("D"),
+		pix("P");
+
+		private String tipoPagamento;
+		Tipos(String string) {
+			
+		}
+		
+		
+		void PagamentoEnum(String tipo) {
+		setTipoPagamento(tipo);
+		}
+		
+		
+		public String getTipoPagamento() {
+			return tipoPagamento;
+		}
+		
+		public void setTipoPagamento(String tipoPagamento) {
+			this.tipoPagamento = tipoPagamento;
+		}
+	}
 
 }
